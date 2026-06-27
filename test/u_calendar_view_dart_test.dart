10d997e03338dfa6a45c3b15ce02e59b8362147b
Tomohiro-Uchida <uchida001tmhr@gmail.com>
2026-06-27 17:35:45 +0900

    0.5.3+47

--- test/u_calendar_view_dart_test.dart
+++ test/u_calendar_view_dart_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:u_calendar_view_dart/u_calendar_view_dart.dart';
 import 'package:u_calendar_view_dart/u_calendar_view_dart_platform_interface.dart';
-import 'package:u_calendar_view_dart/u_calendar_view_dart_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockUCalendarViewDartPlatform
@@ -13,17 +11,4 @@ class MockUCalendarViewDartPlatform
 }
 
 void main() {
-  final UCalendarViewDartPlatform initialPlatform = UCalendarViewDartPlatform.instance;
-
-  test('$MethodChannelUCalendarViewDart is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelUCalendarViewDart>());
-  });
-
-  test('getPlatformVersion', () async {
-    UCalendarViewDart uCalendarViewDartPlugin = UCalendarViewDart();
-    MockUCalendarViewDartPlatform fakePlatform = MockUCalendarViewDartPlatform();
-    UCalendarViewDartPlatform.instance = fakePlatform;
-
-    // expect(await uCalendarViewDartPlugin.getPlatformVersion(), '42');
-  });
 }
